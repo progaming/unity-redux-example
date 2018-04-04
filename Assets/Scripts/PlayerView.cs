4273b2eec763ffc112753211ed65a5c5d8a233e7
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-04-04 14:18:22 +0700

    add win condition

--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -11,15 +11,18 @@ public class PlayerView : MonoBehaviour {
 
 	private ActionCreator _actionCreator;
 	private Player _player;
+	private Enemy _enemy;
 
 	void Start() {
 		_player = store.GetComponent<Player>();
+		_enemy = store.GetComponent<Enemy>();
 		_actionCreator = action.GetComponent<ActionCreator>();
 
 		_player.hp.SubscribeToText(hpText);
 		
 		this.UpdateAsObservable()
 			.Where( _ => Input.GetKeyUp(KeyCode.Space))
+			.Where( _ => _enemy.hp.Value > 0)
 			.Subscribe( _ =>  _actionCreator.Attack(AttackTarget.ENEMY, 1));
 	}
 }
