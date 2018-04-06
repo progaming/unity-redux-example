df8595b589e7a1acb287550fa74c29b4ca44c734
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-04-06 12:43:13 +0700

    update example scene

--- Assets/Scripts/Gameplay/EnemyView.cs
+++ Assets/Scripts/Gameplay/EnemyView.cs
@@ -7,24 +7,16 @@ using ReduxExample.Gameplay.Actions;
 
 namespace ReduxExample.Gameplay {
 	public class EnemyView : MonoBehaviour {
-		public GameObject redux;
+		public Creator creator;
+		public Store store;
 		public Text hpText;
 
-		private Store _store;
-		private Creator _creator;
-
-		void Awake() {
-			_creator = redux.GetComponent<Creator>();
-			_store = redux.GetComponent<Store>();
-		}
-
 		void Start() {
-
-			_store.Enemy.hp.SubscribeToText(hpText);
+			store.Enemy.hp.SubscribeToText(hpText);
 
 			this.UpdateAsObservable()
 				.ThrottleFirstFrame(60)
-				.Subscribe( _ =>  _creator.Fight.Attack(AttackTarget.PLAYER, 1));
+				.Subscribe( _ =>  creator.Fight.Attack(AttackTarget.PLAYER, 1));
 		}
 	}
 }
--- Assets/Scripts/Gameplay/PlayerView.cs
+++ Assets/Scripts/Gameplay/PlayerView.cs
@@ -7,24 +7,17 @@ using ReduxExample.Gameplay.Actions;
 
 namespace ReduxExample.Gameplay {
 	public class PlayerView : MonoBehaviour {
-		public GameObject redux;
+		public Creator creator;
+		public Store store;
 		public Text hpText;
 
-		private Store _store;
-		private Creator _creator;
-
-		void Awake() {
-			_creator = redux.GetComponent<Creator>();
-			_store = redux.GetComponent<Store>();
-		}
-
 		void Start() {
-			_store.Player.hp.SubscribeToText(hpText);
+			store.Player.hp.SubscribeToText(hpText);
 			
 			this.UpdateAsObservable()
 				.Where( _ => Input.GetKeyUp(KeyCode.Space))
-				.Where( _ => _store.Enemy.hp.Value > 0)
-				.Subscribe( _ =>  _creator.Fight.Attack(AttackTarget.ENEMY, 1));
+				.Where( _ => store.Enemy.hp.Value > 0)
+				.Subscribe( _ =>  creator.Fight.Attack(AttackTarget.ENEMY, 1));
 		}
 	}
 }
