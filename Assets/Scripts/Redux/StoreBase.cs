6f7c0b60a01ad08a8c9d012a6726ec79f725c594
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-05-04 22:40:47 +0700

    support middleware

--- Assets/Scripts/Redux/StoreBase.cs
+++ Assets/Scripts/Redux/StoreBase.cs
@@ -3,18 +3,22 @@ using System.Linq;
 
 public class StoreBase
 {
-    private List<IReducer> _reducers;
-    public void Init()
+    private IMiddleware[] middlewares;
+    private List<IReducer> reducers;
+
+    public void Init(params IMiddleware[] middlewares)
     {
-        _reducers = this.GetType().GetFields()
+        this.middlewares = middlewares;
+        reducers = this.GetType().GetFields()
             // Include only the properties that implement IReducer
             .Where(f => f.GetValue(this).GetType().GetInterfaces().Contains(typeof(IReducer)))
             // Cast to IReducer and convert them to list
             .Select(f => f.GetValue(this) as IReducer)
             .ToList();
     }
-    public void Dispatch(object payload)
+    public void Dispatch(object action)
     {
-        _reducers.ForEach(r => r.Reduce(payload));
+        middlewares.ToList().ForEach(m => action = m.Apply(action));
+        reducers.ForEach(r => r.Reduce(action));
     }
 }
--- Assets/Scripts/Redux/Types.cs
+++ Assets/Scripts/Redux/Types.cs
@@ -2,3 +2,8 @@
 {
     void Reduce(object payload);
 }
+
+public interface IMiddleware
+{
+    object Apply(object action);
+}
