7cd3a02c1840f26a73b4b7465d9388cf418ee7c2
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-05-04 22:51:11 +0700

    refactor

--- Assets/Scripts/Redux/Types.cs
+++ Assets/Scripts/Redux/Types.cs
@@ -1,9 +1,9 @@
 ﻿public interface IReducer
 {
-    void Reduce(object payload);
+    void Reduce(dynamic payload);
 }
 
 public interface IMiddleware
 {
-    object Apply(object action);
+    dynamic Apply(dynamic action);
 }
